b0458d168ee83178ff5ac2ba79eb023f77dd63fa
xnlongit <xnlongit@gmail.com>
2025-01-16 15:14:09 +0700

    update middleware

--- server/src/route/api.js
+++ server/src/route/api.js
@@ -3,28 +3,34 @@ import workspaceController from "../controllers/workspaceController";
 import userController from "../controllers/userController";
 import projectController from "../controllers/projectController";
 import taskController from "../controllers/taskController";
+import checkAuthentication from '../middlewares/checkAuthentication';
 
 let router = express.Router();
 
 const initAPIRoute = (app) => {
-
+    // Public routes
     router.post('/create-user', userController.createUser);
     router.post('/login', userController.handleLogin);
-    router.get('/users', userController.getAllUsers);
 
-    router.post('/create-workspace', workspaceController.createWorkspace);
-    router.get('/users/:userId/workspaces', workspaceController.getWorkspacesByUserId);
-    router.get('/workspaces', workspaceController.getAllWorkspaces);
-    router.put('/update-workspaces/:workspaceId', workspaceController.updateWorkspace);
-    router.delete('/delete-workspaces/:workspaceId', workspaceController.deleteWorkspace);
+    // Protected routes
+    router.get('/users', checkAuthentication, userController.getAllUsers);
 
-    router.post('/create-project', projectController.createProject);
-    router.get('/workspace/:workspaceId/projects', projectController.getProjectsByWorkspaceId);
-    router.put('/update-projects/:projectId', projectController.updateProject);
-    router.delete('/delete-projects/:projectId', projectController.deleteProject);
+    // Workspace routes
+    router.post('/create-workspace', checkAuthentication, workspaceController.createWorkspace);
+    router.get('/users/:userId/workspaces', checkAuthentication, workspaceController.getWorkspacesByUserId);
+    router.get('/workspaces', checkAuthentication, workspaceController.getAllWorkspaces);
+    router.put('/update-workspaces/:workspaceId', checkAuthentication, workspaceController.updateWorkspace);
+    router.delete('/delete-workspaces/:workspaceId', checkAuthentication, workspaceController.deleteWorkspace);
 
-    router.post('/create-task', taskController.createTask);
-    router.get('/workspace/projects/:projectId/tasks', taskController.getTasksByProjectId);
+    // Project routes
+    router.post('/create-project', checkAuthentication, projectController.createProject);
+    router.get('/workspace/:workspaceId/projects', checkAuthentication, projectController.getProjectsByWorkspaceId);
+    router.put('/update-projects/:projectId', checkAuthentication, projectController.updateProject);
+    router.delete('/delete-projects/:projectId', checkAuthentication, projectController.deleteProject);
+
+    // Task routes
+    router.post('/create-task', checkAuthentication, taskController.createTask);
+    router.get('/workspace/projects/:projectId/tasks', checkAuthentication, taskController.getTasksByProjectId);
 
     return app.use("/api/v1/", router);
 };
